7463ffbab7b584491896c739d41c75e7dec469b2
unknown <maria.torres-m@labinfo.is.escuelaing.edu.co>
2025-03-07 16:30:41 -0500

    Despliegue{

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -12,11 +12,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.qos.logback.classic.Logger;
 import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
+import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
 import edu.eci.arsw.blueprints.services.BlueprintsServices;
 
 /**
@@ -37,10 +43,49 @@ public class BlueprintAPIController {
         try{
             Collection<Blueprint> blueprints = bpp.getAllBlueprints();
             return new ResponseEntity<>(blueprints,HttpStatus.ACCEPTED);
-        }catch (Exception e){
-            e.printStackTrace();
+        }catch (Exception e){            
             return new ResponseEntity<>("No se encontro los planos",HttpStatus.NOT_FOUND);
         }
     }
+
+
+    @GetMapping("/{author}")
+    public ResponseEntity<?> getBlueprint(@PathVariable String author) {
+        try {
+            Set <Blueprint> blueprints = bpp.getBlueprintsByAuthor(author);
+            return new ResponseEntity<>(blueprints,HttpStatus.ACCEPTED);
+        }catch (BlueprintNotFoundException e) {
+            return new ResponseEntity<>("No se encontro los planos del autor:"+author,HttpStatus.NOT_FOUND);
+        }catch (Exception m){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{author}/{name}")
+    public ResponseEntity<?> getBlueprint(@PathVariable String author, @PathVariable String name) {
+        try {
+            Blueprint blueprint = bpp.getBlueprint(author, name);
+            return new ResponseEntity<>(blueprint,HttpStatus.ACCEPTED);
+        } catch (BlueprintNotFoundException e) {
+            return new ResponseEntity<>("No se encontro los planos",HttpStatus.NOT_FOUND);
+        }catch (Exception m){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> postMethodName(@RequestBody Blueprint blueprint)  {
+        try {
+            bpp.addNewBlueprint(blueprint);
+            return new ResponseEntity<>(blueprint,HttpStatus.ACCEPTED);
+        } catch (BlueprintPersistenceException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("No se guardo el plano",HttpStatus.NOT_FOUND);
+        }catch (Exception m){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
+    }
+
 }
 
