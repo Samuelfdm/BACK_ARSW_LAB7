2f25950921d4d595948fa425bdc8a90ad038a60d
Mar972310 <maria.torres-m@mail.escuelaing.edu.co>
2025-03-07 05:18:28 -0500

    Afanmm

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -5,22 +5,42 @@
  */
 package edu.eci.arsw.blueprints.controllers;
 
+import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
 
 /**
  *
  * @author hcadavid
  */
+@RestController
+@RequestMapping(value =  "/blueprints")
+
 public class BlueprintAPIController {
-    
-    
-    
-    
-    
+
+    @Autowired
+    BlueprintsServices bpp;
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> allBlueprints(){
+        try{
+            Collection<Blueprint> blueprints = bpp.getAllBlueprints();
+            return new ResponseEntity<>(blueprints,HttpStatus.ACCEPTED);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>("No se encontro los planos",HttpStatus.NOT_FOUND);
+        }
+    }
 }
 
